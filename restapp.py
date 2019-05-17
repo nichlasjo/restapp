1a8a189ce1aba67c9af20176ef88ab3cd1aa389f
Nichlas Ekman <nickysoulless@gmail.com>
2019-05-17 22:06:33 +0200

    get created json files

--- restapp.py
+++ restapp.py
@@ -9,7 +9,7 @@ api = Api(app)
 
 class Root_Message(Resource):
     def get(self):
-        return 'Welcome to test RestApi v0.2'
+        return 'Welcome to test RestApi v0.1'
 
 class Dir_Objects(Resource):
     def get(self, list):
@@ -39,9 +39,20 @@ class File_POST(Resource):
             result = json.dump(data, f, indent=4)
         return data
 
+class File_OPEN(Resource):
+    def get(self, data):
+        result = {}
+        current_path = str(request.path)[1:]
+        output = current_path.split('/')[1:][0]
+        json_file = current_path + '.json'
+        with open(json_file,'rb') as f:
+            result = json.load(f)
+        return result
+
 api.add_resource(Root_Message, '/')
 api.add_resource(Dir_Objects, '/<string:list>/', methods=['GET'])
 api.add_resource(File_POST, '/data/<string:data>', methods=['POST'])
+api.add_resource(File_OPEN, '/data/<string:data>', methods=['GET'])
 api.add_resource(File_GET, '/conf/<string:data>/', methods=['GET'])
 
 if __name__ == '__main__':
